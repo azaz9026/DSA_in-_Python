65f5ff57d11ad5232f5bd6cb50832c5815633864
Md Azaz <mdazaz9026@gmail.com>
2023-10-14 23:14:15 +0530

    s

--- binarySearching.py
+++ binarySearching.py
@@ -1,24 +1,38 @@
 ## 1 Binary Search -----------------------------------------------------------------------------------------
 
+
 ## In a Sorted Array 
 
 
+
 def BinarySearch(arr , i , j , x):
+
     while i <= j:
+
         midpoint = i+(j-i)//2
+
         if arr[midpoint]==x:
             return midpoint
+
         elif arr[midpoint]<x:
+
             return BinarySearch(arr , midpoint+1 , j , x)
+
         else:
+
             return BinarySearch(arr , i , midpoint-1 , x)
     return -1
                
+
 
 
 arr = [10,20,30,40,50,60,70,80,90,100]
 x = 100
 i = 0
 j = len(arr)-1
+
 result = BinarySearch(arr , i , j , x )
+
 print(result)
+
+
