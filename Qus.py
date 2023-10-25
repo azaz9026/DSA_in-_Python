4b3781255b8a598a554febb2cd2a33ee46e75845
Md Azaz <mdazaz9026@gmail.com>
2023-10-25 21:12:00 +0530

    sortColor

--- Qus.py
+++ Qus.py
@@ -22,7 +22,8 @@ result = findMaxProfit(prices)
 print(" the max_ profit is to be this one " , result)
 
 ## the Ans Should be 14 ---------------------->  the max_ profit is to be this one  14
-
+## Time Complexity O(n)
+## Space Complexity O(1)
 
 
 ## /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,7 +47,8 @@ x1 , x2 , x3 , y1 , y2 , y3 = 1 , 1 , 1 , 6 , 0 , 9
 collinearPoint(x1 , x2 , x3 , y1 , y2 , y3)
 
 ## the Ans Should be  ---------------------->  The given point are Collinear.
-
+## Time Complexity O(1)
+## Space Complexity O(1)
 
 
 
@@ -77,3 +79,45 @@ y3 = 9
 ## Function Calling 
 
 collinearPoint2(x1,x2,x3,y1,y2,y3)
+
+## the Ans Should be  ---------------------->  The given point are Collinear.
+## Time Complexity O(1)
+## Space Complexity O(1)
+
+
+## /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+## (Qus :- 4)  Collinear point other method 
+
+
+## Function Deffinition
+
+def sortColor(num):
+    p0 = 0 
+    p2 = len(num)-1
+    curr = 0
+    while curr <= p2:
+        if num[curr] == 0:
+            # Swap num(curr) and num(p0)
+            num[curr] , num[p0] = num[p0] , num[curr]
+            curr += 1
+            p0 += 1
+        elif num[curr] == 2:
+            # Swap num(curr) and num(p2)
+            num[curr] , num[p2] = num[p2] , num[curr]
+            p2 -= 1
+        else:
+            curr += 1 
+    return num
+    
+
+
+## Driver Code
+num = [2,0,2,1,1,0]
+color = sortColor(num)
+print(color)
+
+## the Ans Should be  ---------------------->  [0, 0, 1, 1, 2, 2] all color are sorted.
+## Time Complexity O(n)
+## Space Complexity O(1)
