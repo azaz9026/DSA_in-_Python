6630b1535a80672d2093b7114140a88021c583f2
Md Azaz <mdazaz9026@gmail.com>
2023-11-24 20:45:12 +0530

    fibonacci series using recurision

---
+++ fibonacci_Series.py
@@ -0,0 +1,17 @@
+## fibonacci series using recurision :-
+
+def fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+n = 10
+for i in range(n):
+    print(fibonacci(i))  
+
+
+## Ans be that 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13 , 21 , 34
+
+
+    
