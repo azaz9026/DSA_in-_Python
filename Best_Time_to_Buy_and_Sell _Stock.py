a2e766820602c4d90f555b23dfcbfd9c105135a2
Md Azaz <mdazaz9026@gmail.com>
2023-11-30 20:40:47 +0530

    anpther method

--- Best_Time_to_Buy_and_Sell _Stock.py
+++ Best_Time_to_Buy_and_Sell _Stock.py
@@ -17,3 +17,23 @@ n = len(prices)
 
 res = max_profit(prices, n)
 print(res)
+
+
+
+
+
+
+
+def max_profit(prices, n):
+    maximum = 0
+    for i in range(0, n-1):
+        for j in range(i+1, n):  # Start from i+1
+            maximum = max(maximum, prices[j] - prices[i])
+        
+    return maximum
+
+prices = [7, 1, 5, 3, 6, 4]
+n = len(prices)
+
+result = max_profit(prices, n)
+print(result) 
