e3d78aedced703756cc10ecfd3cca237e85a3fb6
Md Azaz <mdazaz9026@gmail.com>
2023-11-30 22:10:01 +0530

    maximum and minimum element in  an array

--- Best_Time_to_Buy_and_Sell _Stock.py
+++ Best_Time_to_Buy_and_Sell _Stock.py
@@ -7,7 +7,7 @@ def max_profit(prices, n):
     for i in range(1, n):
         profit = max(profit, prices[i] - minimum)
         minimum = min(minimum, prices[i])
-    return profit
+    return profit , minimum
 
 prices = [7, 1, 5, 3, 6, 4]
 n = len(prices)
@@ -34,3 +34,28 @@ n = len(prices)
 
 result = max_profit(prices, n)
 print(result) 
+
+
+
+def max_profit(prices , n):
+    min_p = prices[0]
+    profit_1 = 0
+    if len(prices) == 0:
+        return 0
+    for i in range(1 , n):
+        if min_p > prices[i]:
+            min_p = prices[i]
+        elif profit_1 < prices[i] - min_p:
+            profit_1 = prices[i] - min_p
+    return profit_1
+
+
+prices = [7, 1, 5, 3, 6, 4]
+n = len(prices)
+
+result = max_profit(prices, n)
+print(result) 
+
+
+
+
---
+++ maximum_and_minimum_element_in_an_array.py
@@ -0,0 +1,28 @@
+#maximum and minimum element in  an array
+
+def max_min(arr , n):
+    min_val = 0
+    max_val = 0
+    if arr[0] < arr[1]:
+        max_val = arr[1]
+        min_val = arr[0]
+    else:
+        max_val = arr[0]
+        min_val = arr[1]
+    
+    for i in range( 2  , n):
+        if arr[i] > max_val:
+            max_val = arr[i]
+        if arr [i] < min_val:
+            min_val = arr[i]
+    
+    return max_val , min_val
+
+
+
+
+arr = [7, 1, 5, 3, 6, 10]
+n = len(arr)
+
+result = max_min(arr, n)
+print(result)
