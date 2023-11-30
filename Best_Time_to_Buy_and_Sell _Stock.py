2dd354ed62a1f6f2c7bec965e3d4304f2b3f1db1
Md Azaz <mdazaz9026@gmail.com>
2023-11-30 21:05:42 +0530

    another method

--- Best_Time_to_Buy_and_Sell _Stock.py
+++ Best_Time_to_Buy_and_Sell _Stock.py
@@ -2,15 +2,12 @@
 ##Best Time to Buy and Sell Stock
 
 def max_profit(prices, n):
-    buy = prices[0]
-    maxi = 0
-    
-    for i in range(n):
-        if buy < prices[i]:
-            buy = prices[i]
-        elif maxi < buy - prices[i]:
-            maxi = buy - prices[i]
-    return maxi
+    minimum = prices[0]
+    profit = 0
+    for i in range(1, n):
+        profit = max(profit, prices[i] - minimum)
+        minimum = min(minimum, prices[i])
+    return profit
 
 prices = [7, 1, 5, 3, 6, 4]
 n = len(prices)
