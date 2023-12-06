5b768856f2b38eeca7f3102c7d80815aa58117e1
Md Azaz <mdazaz9026@gmail.com>
2023-12-06 22:47:30 +0530

    ## another Method

--- Array/Maximum_Subarray.py
+++ Array/Maximum_Subarray.py
@@ -2,6 +2,28 @@
 
 
 def MaximumArray(arr):
+    
+    maxi = arr[0]
+    n = len(arr)
+
+    for i in range(n):
+        curr_Sum = 0
+        for j in range(i, n):
+            curr_Sum += arr[j]
+            maxi = max(maxi , curr_Sum)
+    
+    return maxi
+
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+result = MaximumArray(arr)
+print(result)
+
+
+## another Method-----------------------------------------------------------------------------------------------------------------------
+
+
+
+def MaximumArray(arr):
     sum = 0
     maxi = arr[0]
 
