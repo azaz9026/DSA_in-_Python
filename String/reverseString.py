eef0a6fcde4c1963619ea8e0ae857fe508c60b5a
Md Azaz <mdazaz9026@gmail.com>
2023-12-06 21:40:47 +0530

    another method

--- String/reverseString.py
+++ String/reverseString.py
@@ -15,8 +15,34 @@ def reverseString(Str):
 
     return ''.join(Str)
 
+## Function Calling
+
+Str = "Azaz khan"
+result = reverseString(Str)
+print(result)
 
 
+
+## another Method-----------------------------------------------------------------------------------------------------------------------
+
+def reverseString(s):
+    # Convert the string to a list
+    str_list = list(s)
+
+    low = 0
+    high = len(str_list) - 1
+
+    while low < high:
+        temp = str_list[low]
+        str_list[low] = str_list[high]
+        str_list[high] = temp
+        low += 1
+        high -= 1
+
+    # Join the list back into a string
+    reversed_str = ''.join(str_list)
+
+    return reversed_str
 
 Str = "Azaz khan"
 result = reverseString(Str)
